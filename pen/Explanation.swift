981bf07378e9cab7c4a6b48b7e038c470ee99832
hikaru777 <takeru.hikaru012277@gmail.com>
2021-05-28 06:27:13 +0900

    comment

---
+++ pen/Explanation.swift
@@ -0,0 +1,40 @@
+//
+//  Explanation.swift
+//  pen
+//
+//  Created by Hikaru.Honda on 2021/05/28.
+//  Copyright © 2021 Hikaru.Honda. All rights reserved.
+//
+
+import Foundation
+
+class Ex {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
